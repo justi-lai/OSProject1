fba5634fbe046c1bf1e35e3057416ebfb50bc0d7
Justin Lai <justin88.lai@gmail.com>
2024-10-06 22:59:46 -0500

    Logger complete

--- logger.py
+++ logger.py
@@ -4,18 +4,17 @@ import datetime
 def write(log_file, action, message):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     entry = f"{timestamp} [{action}] {message}"
-    with open(log_file, mode='w') as file:
+    with open(log_file, mode='a') as file:
         file.write(entry + '\n')
 
 def process_log(log_file):
     while True:
         log = sys.stdin.readline().rstrip()
-
-        if log == 'QUIT':
-            break
-
         log_parts = log.split(maxsplit=1)
-        action = log_parts[0]
+        action = log_parts[0].upper()
+        if action == 'QUIT':
+            write(log_file, action, '')
+            break
         message = log_parts[1] if len(log_parts) > 1 else ''
 
         write(log_file, action, message)
