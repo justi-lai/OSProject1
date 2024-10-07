e0bc19f2a8bea83a8b04911c3d3bd5b5e5547657
Justin Lai <justin88.lai@gmail.com>
2024-10-07 00:33:41 -0500

    Password function on driver working, not implemented on encryption yet so not fully functioning

--- driver.py
+++ driver.py
@@ -25,6 +25,7 @@ def logMessage(process, action, message=''):
     process.stdin.flush()
 
 def menu():
+    print('\n***************')
     print('\nMenu')
     print('1. PASSWORD')
     print('2. ENCRYPT')
@@ -32,8 +33,26 @@ def menu():
     print('4. HISTORY')
     print('5. QUIT')
 
-def password():
-    # temp
+def password(process, passwords):
+    print('\nPASSWORD')
+    print('1. NEW PASSWORD')
+    print('2. HISTORY')
+    print('3. BACK')
+
+    command = input().rstrip().upper()
+    while command != '3' and command != 'BACK':
+        if command == 1 or command == 'NEW' or command == 'NEW PASSWORD':
+            password = input('NEW PASSWORD: ')
+            passwords.append(password)
+        elif command == '2' or command == 'HISTORY':
+            print('Choose a password:')
+            for i in range(1, len(passwords)+1):
+                print(i + '. ' + passwords[i])
+            command = input().rstrip()
+            password = passwords[int(command)]
+    
+    process.stdin.write(password)
+    process.stdin.flush()
 
 def encrypt():
 
@@ -46,18 +65,23 @@ def history():
 
 def main(logFile):
     logger, encryption = startProcesses(logFile)
+
+    passwords = []
     
-    command = input().strip()
-    while command is not '5':
+    command = input().strip().upper()
+    while command != '5' and command != 'QUIT':
         menu()
-        if command == 1:
-            password()
-        elif command == 2:
+        if command == '1' or command == 'PASSWORD':
+            password(passwords)
+        elif command == '2' or command == 'ENCRYPT':
             encrypt()
-        elif command == 3:
+        elif command == '3' or command == 'DECRYPT':
             decrypt()
-        else:
+        elif command == '4' or command == 'HISTORY':
             history()
+        else:
+            print('ERROR: Unknown input. Please try again.')
+        command = input().strip().upper()
     
     logMessage(logger, 'QUIT')
     
