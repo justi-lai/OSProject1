04aeab8cb03c4a292e9ead7e84d54db58877afa0
Justin Lai <justin88.lai@gmail.com>
2024-10-06 19:34:01 -0500

    10/6/2024: Opened processes for logger.py and encryption.py.

--- driver.py
+++ driver.py
@@ -1,1 +1,34 @@
+import subprocess
+import os
+import sys
 
+def startProcesses(logFile):
+    loggerProcess = subprocess.Popen(
+        ['python3', './logger.py', logFile],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    encryptionProcess = subprocess.Popen(
+        ['python3', './encryption.py'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    # loutput, lerrors = loggerProcess.communicate()
+    # eoutput, eerrors = encryptionProcess.communicate()
+    # print(loutput)
+    # print(eoutput)
+
+def main(logFile):
+    startProcesses(logFile)
+
+if __name__ == 'main':
+    if len(sys.argv) != 2:
+        print("Usage: python3 driver.py <log_file>")
+        sys.exit(1)
+    
+    log_file = sys.argv[1]
+    main(log_file)
