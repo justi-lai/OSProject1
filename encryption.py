fc715493a2f44c4d131d49634e1532f911f69154
Justin Lai <justin88.lai@gmail.com>
2024-10-13 02:40:42 -0500

    Encryption.py full functionality added

--- encryption.py
+++ encryption.py
@@ -19,16 +19,66 @@ def main():
         message = log_parts[1] if len(log_parts) > 1 else ''
 
         if action == 'PASSKEY':
-            passkey = message
-            print('e test')
+            passkey = message.upper()
             output()
-            print('encryption test')
-        # elif action == 'ENCRYPT':
-        #     body
-        # elif action == 'DECRYPT':
-        #     body
+        
+        elif action == 'ENCRYPT':
+            if passkey == '':
+                output('ERROR', 'Password not set')
+            else:
+                result = ''
+                key_idx = 0
+                for character in message:
+                    if character.isalpha():
+                        char_code = ''
+                        if character.isupper():
+                            char_code = ord(character) - ord('A')
+                        else:
+                            char_code = ord(character) - ord('a')
+                        key_code = ord(passkey[key_idx % len(passkey)])
+                        shift = key_code - ord('A')
+
+                        encrypted_character = ''
+                        if character.isupper():
+                            encrypted_character = chr(((char_code + shift) % 26) + ord('A'))
+                        else:
+                            encrypted_character = chr(((char_code + shift) % 26) + ord('a'))
+                        
+                        result += encrypted_character
+                        key_idx += 1
+                    else:
+                        result += character
+                output(message=result)
+
+        elif action == 'DECRYPT':
+            if passkey == '':
+                output('ERROR', 'Password not set')
+            else:
+                result = ''
+                key_idx = 0
+                for character in message:
+                    if character.isalpha():
+                        char_code = ''
+                        if character.isupper():
+                            char_code = ord(character) - ord('A')
+                        else:
+                            char_code = ord(character) - ord('a')
+                        key_code = ord(passkey[key_idx % len(passkey)])
+                        shift = key_code - ord('A')
+
+                        encrypted_character = ''
+                        if character.isupper():
+                            encrypted_character = chr(((char_code - shift) % 26) + ord('A'))
+                        else:
+                            encrypted_character = chr(((char_code - shift) % 26) + ord('a'))
+                        
+                        result += encrypted_character
+                        key_idx += 1
+                    else:
+                        result += character
+                output(message=result)
         else:
-            print('Error: Invalid Input')
+            output('ERROR', 'Unrecognized input')
 
 if __name__ == '__main__':
     if len(sys.argv) != 1:
